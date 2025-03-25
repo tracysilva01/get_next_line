5af42fa69feb7fe3f9f65c1795006913f2667790
Tracy Emperatriz Silva Flores <trsilva-@c2r17s4.42madrid.com>
2025-03-25 23:59:38 +0100

    restarted again, major changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,49 +6,50 @@
 /*   By: trsilva- <trsilva-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 20:48:14 by trsilva-          #+#    #+#             */
-/*   Updated: 2025/03/25 19:11:12 by trsilva-         ###   ########.fr       */
+/*   Updated: 2025/03/25 23:53:31 by trsilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*fill_line(int fd)
+char	*read_and_store(int fd)
 {
 	char	*buffer;
-	char	*temp;
-	char	*new_temp;
 	int		bytes_read;
 
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	temp = ft_strdup("");
-	while (!ft_strchr(temp, '\n'))
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read <= 0)
-		{
-			free(buffer);
-			if (bytes_read == 0)
-			{
-				if (*temp)
-					return (temp);
-				else
-				{
-					free(temp);
-					return (NULL);
-				}
-			}
-			free(temp);
+		free(buffer);
+		if (bytes_read == 0)
 			return (NULL);
-		}
-		buffer[bytes_read] = '\0';
-		new_temp = ft_strjoin(temp, buffer);
-		free(temp);
-		temp = new_temp;
+		return (NULL);
 	}
-	free(buffer);
-	return (temp);
+	buffer[bytes_read] = '\0';
+	return (buffer);
+}
+
+char	*fill_line(int fd, char *remains)
+{
+	char	*line;
+	char	*buffer;
+	char	*temp;
+
+	line = ft_strdup(remains);
+	while (!ft_strchr(line, '\n'))
+	{
+		buffer = read_and_store(fd);
+		if (!buffer)
+			break ;
+		temp = ft_strjoin(line, buffer);
+		free(line);
+		free(buffer);
+		line = temp;
+	}
+	return (line);
 }
 
 char	*fill_remains(char *line)
@@ -80,19 +81,17 @@ char	*get_next_line(int fd)
 {
 	static char	*remains;
 	char		*line;
-	char		*temp;
 
 	if (!remains)
 		remains = ft_strdup("");
-	temp = fill_line(fd);
-	if (!temp)
+	line = fill_line(fd, remains);
+	if (!line || !*line)
 	{
+		free(line);
 		free(remains);
 		remains = NULL;
 		return (NULL);
 	}
-	line = ft_strjoin(remains, temp);
-	free(temp);
 	free(remains);
 	remains = fill_remains(line);
 	return (remove_remains(line));
@@ -109,14 +108,16 @@ int	main(void)
 		perror("Error opening file");
 		return (1);
 	}
-	while ((line = get_next_line(fd)))
+	line = get_next_line(fd);
+	while (line)
 	{
-		printf("%s", line);
+		printf("LINEA LEIDA:%s", line);
 		free(line);
+		line = get_next_line(fd);
 	}
 	close(fd);
 	return (0);
 }
 
-// gcc -g -o mi_programa get_next_line_utils.c get_next_line.c
-// valgrind --leak-check=full ./mi_programa
+//gcc -g -o mi_programa get_next_line_utils.c get_next_line.c
+//valgrind --leak-check=full ./mi_programa*/
